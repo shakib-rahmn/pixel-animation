ca360529bce241a79b6fdf36cfad7b265b617f34
Shakib Rahman <archneed01@gmail.com>
2023-10-26 00:21:25 +0600

    refactored

--- app.js
+++ app.js
@@ -11,18 +11,25 @@ const mainBox = {
 // total pixel boxes
 const totalPixel = mainBox.countPixel();
 
-// calculating width
-const pixelWidth = (mainBox.box.clientWidth - mainBox.totalCol - 1) / mainBox.totalCol;
+// calculating dimension
+const pixelArea = (mainBox.box.clientWidth - mainBox.totalCol - 1) / mainBox.totalCol;
 
-// array for all index
-const pixelsArray = [];
+// function to add css styles
+const addCss = (element, styles) => {
+  Object.assign(element.style, styles);
+}
 
 // creating pixel boxes
 for (let i = 0; i < totalPixel; i++) {
-  pixelsArray.push(i);
   let newDiv = document.createElement("div");
-  newDiv.style.width = `${pixelWidth}px`;
-  newDiv.style.height = `${pixelWidth}px`;
+
+  let styles = {
+    width: `${pixelArea}px`,
+    height: `${pixelArea}px`,
+  }
+
+  addCss(newDiv, styles);
+
   newDiv.classList.add("pixel");
   mainBox.box.appendChild(newDiv);
 }
@@ -35,17 +42,15 @@ let randomDigit = (value) => {
   return Math.ceil(Math.random() * value);
 };
 
-// shuffling index
-pixelsArray.sort(() => randomDigit(0.1));
+// generating css styles
+pixelsList.forEach(pixel => {
+  pixel.classList.add('animate');
 
-pixelsArray.forEach(index => {
-  pixelsList[index].classList.add('animate');
-
-  let style = {
+  let styles = {
     animationDuration: `${randomDigit(4)+1}s`,
     animationDelay: `${randomDigit(6000)+1000}ms`,
     backgroundColor: `hsl(0, 0%, ${randomDigit(64)}%)`,
   }
 
-  Object.assign(pixelsList[index].style, style);
+  addCss(pixel, styles);
 })
